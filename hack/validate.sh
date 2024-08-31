a48087d963e9a1e10652d09352ff82a74c790e76
Heathcliff <heathcliff@heathcliff.eu>
2024-08-31 15:05:27 +0200

    validate: Check that go code is formatted
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/validate.sh
+++ hack/validate.sh
@@ -7,6 +7,15 @@ set -e
 script_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)"
 base_dir="${script_dir}/.."
 
+echo "Check if source code is formatted"
+make fmt
+rc=0
+git update-index --refresh && git diff-index --quiet HEAD -- || rc=1
+if [ $rc -ne 0 ]; then
+    echo "FATAL: Need to run \"make fmt\""
+    exit 1
+fi
+
 echo "Check if the auto generated code is up to date"
 "${script_dir}/generate.sh"
 
