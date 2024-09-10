cb3c9a7878a9531466579d3fdb0171053a47e996
Heathcliff <heathcliff@heathcliff.eu>
2024-09-10 22:39:01 +0200

    coverprofiles: Only run for ./pkg

--- hack/coverprofile.sh
+++ hack/coverprofile.sh
@@ -13,7 +13,7 @@ if [ ! -d "${OUT_DIR}" ]; then
     mkdir "${OUT_DIR}"
 fi
 
-go test -coverprofile="${OUT_DIR}/cover-${APP}.out.tmp" -coverpkg "./..." "./..."
+go test -coverprofile="${OUT_DIR}/cover-${APP}.out.tmp" -coverpkg "./pkg/..." "./pkg/..."
 cat "${OUT_DIR}/cover-${APP}.out.tmp" | grep -v "zz_generated" | grep -v "github.com/heathcliff26/kube-upgrade/pkg/client" > "${OUT_DIR}/cover-${APP}.out"
 rm "${OUT_DIR}/cover-${APP}.out.tmp"
 go tool cover -html "${OUT_DIR}/cover-${APP}.out" -o "${OUT_DIR}/index.html"
