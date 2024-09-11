c4b7c3992bbe0ff4abec58837ceb604fc67f48b6
Heathcliff <heathcliff@heathcliff.eu>
2024-09-11 17:30:47 +0200

    e2e: Restructure e2e tests to allow multiple tests to run sequentially
    Additionally, remove accidentally imported kwok provider.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- tests/e2e_suite_test.go
+++ tests/e2e_suite_test.go
@@ -19,84 +19,10 @@ import (
 	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
-	"sigs.k8s.io/e2e-framework/pkg/types"
 )
 
-func TestBasic(t *testing.T) {
-	testenv.Test(t, featCertManager(), featControllerDeployment(),
-		features.New("create basic crd").
-			Setup(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
-				r, err := resources.New(c.Client().RESTConfig())
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				err = decoder.ApplyWithManifestDir(ctx, r, "manifests/release", "upgrade-cr.yaml", []resources.CreateOption{})
-				if err != nil {
-					t.Fatalf("Failed to apply upgrade-plan manifest: %v", err)
-				}
-
-				return ctx
-			}).
-			Assess("plan", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
-				r, err := resources.New(c.Client().RESTConfig())
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				err = api.AddToScheme(r.GetScheme())
-				if err != nil {
-					t.Fatalf("Failed to add CRD to scheme: %v", err)
-				}
-
-				plan := &api.KubeUpgradePlan{}
-
-				err = r.Get(ctx, "upgrade-plan", "", plan)
-				if err != nil || plan == nil {
-					t.Fatalf("Failed to fetch upgrade-plan: %v", err)
-				}
-
-				err = wait.For(conditions.New(r).ResourceMatch(plan, func(obj k8s.Object) bool {
-					plan, ok := obj.(*api.KubeUpgradePlan)
-					if !ok {
-						return false
-					}
-					return plan.Status.Summary == api.PlanStatusUnknown
-				}), wait.WithTimeout(1*time.Minute), wait.WithInterval(1*time.Second))
-				if err != nil {
-					t.Fatalf("Plan status summary is expected to be %s but is %s: %v", api.PlanStatusUnknown, plan.Status.Summary, err)
-				}
-
-				assert := assert.New(t)
-
-				assert.Equal(len(plan.Spec.Groups), len(plan.Status.Groups), "Should have a status for each group")
-				assert.Equal(api.PlanStatusProgressing, plan.Status.Groups["control-plane"], "control-plane group should be progressing")
-				assert.Equal(api.PlanStatusUnknown, plan.Status.Groups["compute"], "compute group should be unknown")
-
-				return ctx
-			}).
-			Assess("nodes", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
-				nodes := &corev1.NodeList{}
-				err := c.Client().Resources().List(ctx, nodes)
-				if err != nil || nodes.Items == nil {
-					t.Fatalf("Error while getting nodes: %v", err)
-				}
-
-				for _, node := range nodes.Items {
-					if _, ok := node.GetAnnotations()["node-role.kubernetes.io/control-plane"]; ok {
-						value := node.GetAnnotations()[constants.NodeUpgradeStatus]
-						assert.Equalf(t, constants.NodeUpgradeStatusPending, value, "control-plane node %s should have upgrade status annotation")
-					}
-				}
-
-				return ctx
-			}).
-			Feature(),
-	)
-}
-
-func featCertManager() types.Feature {
-	return features.New("deploy cert-manager").
+func TestE2E(t *testing.T) {
+	certManagerFeat := features.New("deploy cert-manager").
 		Setup(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
 			res, err := http.Get(fmt.Sprintf("https://github.com/cert-manager/cert-manager/releases/download/%s/cert-manager.yaml", certManagerVersion))
 			if err != nil || res.StatusCode != http.StatusOK {
@@ -124,10 +50,8 @@ func featCertManager() types.Feature {
 			return ctx
 		}).
 		Feature()
-}
 
-func featControllerDeployment() types.Feature {
-	return features.New("deploy upgrade-controller").
+	controllerDeploymentFeat := features.New("deploy upgrade-controller").
 		Setup(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
 			r, err := resources.New(c.Client().RESTConfig())
 			if err != nil {
@@ -170,4 +94,92 @@ func featControllerDeployment() types.Feature {
 			return ctx
 		}).
 		Feature()
+
+	testenv.Test(t, certManagerFeat, controllerDeploymentFeat)
+	if t.Failed() {
+		t.Fatal("Failed to deploy controller, can't run tests")
+	}
+
+	examplePlanFeat := features.New("create example plan").
+		Setup(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			r, err := resources.New(c.Client().RESTConfig())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			err = decoder.ApplyWithManifestDir(ctx, r, "manifests/release", "upgrade-cr.yaml", []resources.CreateOption{})
+			if err != nil {
+				t.Fatalf("Failed to apply upgrade-plan manifest: %v", err)
+			}
+
+			return ctx
+		}).
+		Assess("status", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			r, err := resources.New(c.Client().RESTConfig())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			err = api.AddToScheme(r.GetScheme())
+			if err != nil {
+				t.Fatalf("Failed to add CRD to scheme: %v", err)
+			}
+
+			plan := &api.KubeUpgradePlan{}
+
+			err = r.Get(ctx, "upgrade-plan", "", plan)
+			if err != nil {
+				t.Fatalf("Failed to fetch upgrade-plan: %v", err)
+			}
+
+			err = wait.For(conditions.New(r).ResourceMatch(plan, func(obj k8s.Object) bool {
+				plan, ok := obj.(*api.KubeUpgradePlan)
+				if !ok {
+					return false
+				}
+				return plan.Status.Summary == api.PlanStatusUnknown
+			}), wait.WithTimeout(1*time.Minute), wait.WithInterval(1*time.Second))
+			if err != nil {
+				t.Fatalf("Plan status summary is expected to be %s but is %s: %v", api.PlanStatusUnknown, plan.Status.Summary, err)
+			}
+
+			assert := assert.New(t)
+
+			assert.Equal(len(plan.Spec.Groups), len(plan.Status.Groups), "Should have a status for each group")
+			assert.Equal(api.PlanStatusProgressing, plan.Status.Groups["control-plane"], "control-plane group should be progressing")
+			assert.Equal(api.PlanStatusUnknown, plan.Status.Groups["compute"], "compute group should be unknown")
+
+			return ctx
+		}).
+		Assess("nodes", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			nodes := &corev1.NodeList{}
+			err := c.Client().Resources().List(ctx, nodes)
+			if err != nil || nodes.Items == nil {
+				t.Fatalf("Error while getting nodes: %v", err)
+			}
+
+			for _, node := range nodes.Items {
+				if _, ok := node.GetAnnotations()["node-role.kubernetes.io/control-plane"]; ok {
+					value := node.GetAnnotations()[constants.NodeUpgradeStatus]
+					assert.Equalf(t, constants.NodeUpgradeStatusPending, value, "control-plane node %s should have upgrade status annotation")
+				}
+			}
+
+			return ctx
+		}).
+		Teardown(func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			r, err := resources.New(c.Client().RESTConfig())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			err = decoder.DeleteWithManifestDir(ctx, r, "manifests/release", "upgrade-cr.yaml", []resources.DeleteOption{})
+			if err != nil {
+				t.Fatalf("Failed to delete upgrade-plan: %v", err)
+			}
+
+			return ctx
+		}).Feature()
+
+	testenv.Test(t, examplePlanFeat)
 }
