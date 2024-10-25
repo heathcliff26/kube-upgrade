51e687ef4e793feae31054087fcf4004d10f3460
Heathcliff <heathcliff@heathcliff.eu>
2024-10-25 17:17:09 +0200

    hack/validate.sh: Ignore kubernetes version update
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/validate.sh
+++ hack/validate.sh
@@ -32,12 +32,16 @@ export TAG="latest"
 export RELEASE_VERSION=""
 "${script_dir}/manifests.sh"
 
+git update-index --refresh || echo "examples/upgrade-controller/upgrade-cr.yaml might only have a kubernetes version update, which will be ignored"
 rc=0
-git update-index --refresh && git diff-index --quiet HEAD -- || rc=1
+git diff-index -I "kubernetesVersion: v1.*" --quiet HEAD -- || rc=1
 if [ $rc -ne 0 ]; then
     echo "FATAL: Need to run \"make manifests\" and update the examples with the result"
     exit 1
 fi
+
+# Ensure an updated kubernetes version is not detected as a problem further down.
+git checkout examples/upgrade-controller/upgrade-cr.yaml
 
 echo "Check if the generated example plan is conform"
 kubeconform -schema-location manifests/generated/kubeupgradeplan_v1alpha2.json -verbose -strict examples/upgrade-controller/upgrade-cr.yaml
