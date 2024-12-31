c8c0466f46d214e957ddd211ccef3efed6f36025
Heathcliff <heathcliff@heathcliff.eu>
2024-12-31 08:20:03 +0100

    hack/coverprofiles.sh: Delete cover.out after generating page
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/coverprofile.sh
+++ hack/coverprofile.sh
@@ -7,15 +7,14 @@ script_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
 pushd "${script_dir}" >/dev/null
 
 OUT_DIR="${script_dir}/coverprofiles"
-APP="kube-upgrade"
 
 if [ ! -d "${OUT_DIR}" ]; then
     mkdir "${OUT_DIR}"
 fi
 
-go test -coverprofile="${OUT_DIR}/cover-${APP}.out.tmp" -coverpkg "./pkg/..." "./pkg/..."
-cat "${OUT_DIR}/cover-${APP}.out.tmp" | grep -v "zz_generated" | grep -v "github.com/heathcliff26/kube-upgrade/pkg/client" > "${OUT_DIR}/cover-${APP}.out"
-rm "${OUT_DIR}/cover-${APP}.out.tmp"
-go tool cover -html "${OUT_DIR}/cover-${APP}.out" -o "${OUT_DIR}/index.html"
+go test -coverprofile="${OUT_DIR}/cover.out.tmp" -coverpkg "./pkg/..." "./pkg/..."
+grep -v "zz_generated" "${OUT_DIR}/cover.out.tmp" | grep -v "github.com/heathcliff26/kube-upgrade/pkg/client" > "${OUT_DIR}/cover.out"
+go tool cover -html "${OUT_DIR}/cover.out" -o "${OUT_DIR}/index.html"
+rm "${OUT_DIR}/cover.out" "${OUT_DIR}/cover.out.tmp"
 
 popd >/dev/null
