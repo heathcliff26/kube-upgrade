c6fffa7d514472b28edaf74b832d47f9522b3801
Heathcliff <heathcliff@heathcliff.eu>
2025-02-01 09:22:48 +0100

    go.mod: Update e2e-framework to v0.6.0
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- tests/main_test.go
+++ tests/main_test.go
@@ -9,7 +9,7 @@ import (
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
 	"sigs.k8s.io/e2e-framework/support/kind"
-	"sigs.k8s.io/e2e-framework/support/utils"
+	"sigs.k8s.io/e2e-framework/pkg/utils"
 )
 
 const namespace = "kube-upgrade"
