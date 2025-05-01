a743cf2cba482d714c95f4496c7dee69c0eb9725
Heathcliff <heathcliff@heathcliff.eu>
2025-05-01 09:01:58 +0200

    Run make generate
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- pkg/client/clientset/versioned/fake/clientset_generated.go
+++ pkg/client/clientset/versioned/fake/clientset_generated.go
@@ -9,6 +9,7 @@ import (
 	fakekubeupgradev1alpha1 "github.com/heathcliff26/kube-upgrade/pkg/client/clientset/versioned/typed/kubeupgrade/v1alpha1/fake"
 	kubeupgradev1alpha2 "github.com/heathcliff26/kube-upgrade/pkg/client/clientset/versioned/typed/kubeupgrade/v1alpha2"
 	fakekubeupgradev1alpha2 "github.com/heathcliff26/kube-upgrade/pkg/client/clientset/versioned/typed/kubeupgrade/v1alpha2/fake"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/discovery"
@@ -36,9 +37,13 @@ func NewSimpleClientset(objects ...runtime.Object) *Clientset {
 	cs.discovery = &fakediscovery.FakeDiscovery{Fake: &cs.Fake}
 	cs.AddReactor("*", "*", testing.ObjectReaction(o))
 	cs.AddWatchReactor("*", func(action testing.Action) (handled bool, ret watch.Interface, err error) {
+		var opts metav1.ListOptions
+		if watchActcion, ok := action.(testing.WatchActionImpl); ok {
+			opts = watchActcion.ListOptions
+		}
 		gvr := action.GetResource()
 		ns := action.GetNamespace()
-		watch, err := o.Watch(gvr, ns)
+		watch, err := o.Watch(gvr, ns, opts)
 		if err != nil {
 			return false, nil, err
 		}
@@ -85,9 +90,13 @@ func NewClientset(objects ...runtime.Object) *Clientset {
 	cs.discovery = &fakediscovery.FakeDiscovery{Fake: &cs.Fake}
 	cs.AddReactor("*", "*", testing.ObjectReaction(o))
 	cs.AddWatchReactor("*", func(action testing.Action) (handled bool, ret watch.Interface, err error) {
+		var opts metav1.ListOptions
+		if watchActcion, ok := action.(testing.WatchActionImpl); ok {
+			opts = watchActcion.ListOptions
+		}
 		gvr := action.GetResource()
 		ns := action.GetNamespace()
-		watch, err := o.Watch(gvr, ns)
+		watch, err := o.Watch(gvr, ns, opts)
 		if err != nil {
 			return false, nil, err
 		}
--- pkg/client/informers/externalversions/kubeupgrade/v1alpha2/kubeupgradeplan.go
+++ pkg/client/informers/externalversions/kubeupgrade/v1alpha2/kubeupgradeplan.go
@@ -46,13 +46,25 @@ func NewFilteredKubeUpgradePlanInformer(client versioned.Interface, namespace st
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.KubeupgradeV1alpha2().KubeUpgradePlans(namespace).List(context.TODO(), options)
+				return client.KubeupgradeV1alpha2().KubeUpgradePlans(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.KubeupgradeV1alpha2().KubeUpgradePlans(namespace).Watch(context.TODO(), options)
+				return client.KubeupgradeV1alpha2().KubeUpgradePlans(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.KubeupgradeV1alpha2().KubeUpgradePlans(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.KubeupgradeV1alpha2().KubeUpgradePlans(namespace).Watch(ctx, options)
 			},
 		},
 		&apiskubeupgradev1alpha2.KubeUpgradePlan{},
