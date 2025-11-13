1cef58ad9be68cabd0b1999fae1aa40870f71ce6
Heathcliff <heathcliff@heathcliff.eu>
2025-11-13 12:12:59 +0100

    upgraded: Remove resolv.conf bind-mount
    Since systemd-resolved is disabled in https://github.com/heathcliff26/containers/pull/432,
    the bind-mount of /etc/resolv.conf is no longer necessary and can be removed.
    
    Fixes: #166
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- cmd/upgraded/start-upgraded.sh
+++ cmd/upgraded/start-upgraded.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# Bind mount the container resolv.conf to prevent issues with systemd-resolved when running kubeadm in chroot
-mount --bind /etc/resolv.conf /host/etc/resolv.conf
-
 # Set the hostname in the pod to the node name.
 # This is needed for kubeadm, as it otherwise fails to find the node name.
 if [ -z "${NODE_NAME}" ]; then
