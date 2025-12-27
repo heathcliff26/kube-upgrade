89e79c3483d914fc35da957d0712a7192a8e1ca1
Heathcliff <heathcliff@heathcliff.eu>
2025-12-27 11:53:52 +0000

    TMP: Certificate
    Signed-off-by: GitHub <noreply@github.com>

--- pkg/upgraded/kubeadm/utils.go
+++ pkg/upgraded/kubeadm/utils.go
@@ -1,6 +1,8 @@
 package kubeadm
 
 import (
+	"crypto/x509"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
@@ -10,6 +12,7 @@ import (
 	"github.com/heathcliff26/kube-upgrade/pkg/upgraded/utils"
 	"github.com/sigstore/sigstore-go/pkg/root"
 	"github.com/sigstore/sigstore-go/pkg/verify"
+	"github.com/sigstore/sigstore/pkg/cryptoutils"
 )
 
 func downloadFile(url, dest string) error {
@@ -42,7 +45,7 @@ func verifySigstoreSignatureCLI(blob, sig, cert string) error {
 	return utils.CreateCMDWithStdout("cosign", "verify-blob", blob, "--signature", sig, "--certificate", cert, "--certificate-identity", "krel-staging@k8s-releng-prod.iam.gserviceaccount.com", "--certificate-oidc-issuer", "https://accounts.google.com").Run()
 }
 
-func verifyArtifactWithSigstore(blob, sig, cert string) error {
+func verifyArtifactWithSigstore(blob, sigURL, certURL string) error {
 	// This bundle uses public good instance with an added signing key
 	trustedRoot, err := root.FetchTrustedRoot()
 	if err != nil {
@@ -52,5 +55,33 @@ func verifyArtifactWithSigstore(blob, sig, cert string) error {
 	if err != nil {
 		panic(err)
 	}
+
 	return nil
+}
+
+func certificateFromURL(url string) (*x509.Certificate, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	raw, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := base64.StdEncoding.DecodeString(string(raw))
+	if err != nil {
+		// not a base64
+		out = raw
+	}
+
+	certs, err := cryptoutils.UnmarshalCertificatesFromPEM(out)
+	if err != nil {
+		return nil, err
+	}
+	if len(certs) == 0 {
+		return nil, fmt.Errorf("no certs found in pem file '%s'", url)
+	}
+	return certs[0], nil
 }
