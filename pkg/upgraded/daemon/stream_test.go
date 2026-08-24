f57e6a8046ba07dea98b5146e2388c8db672f1eb
Heathcliff <heathcliff@heathcliff.eu>
2026-08-24 18:11:24 +0200

    upgraded/daemon: Increase unit-test coverage
    Fill gaps in unit-test coverage by simulating rpm-ostree and kubeadm with
    bash scripts.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- pkg/upgraded/daemon/stream_test.go
+++ pkg/upgraded/daemon/stream_test.go
@@ -1,8 +1,12 @@
 package daemon
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
 	fleetlock "github.com/heathcliff26/fleetlock/pkg/client"
 	rpmostree "github.com/heathcliff26/kube-upgrade/pkg/upgraded/rpm-ostree"
@@ -12,6 +16,89 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
+
+func TestWatchForUpgrade(t *testing.T) {
+	tMatrix := []struct {
+		Name            string
+		CheckExitCode   string
+		UpgradeExitCode string
+		ExpectedCalls   string
+	}{
+		{
+			Name:          "NoUpgrade",
+			ExpectedCalls: "upgrade --check\n",
+		},
+		{
+			Name:          "Upgrade",
+			CheckExitCode: "0",
+			ExpectedCalls: "upgrade --check\nupgrade --reboot\n",
+		},
+		{
+			Name:            "UpgradeFail",
+			CheckExitCode:   "0",
+			UpgradeExitCode: "1",
+			ExpectedCalls:   "upgrade --check\nupgrade --reboot\nupgrade --reboot\n",
+		},
+		{
+			Name:          "CheckFail",
+			CheckExitCode: "1",
+			ExpectedCalls: "upgrade --check\nupgrade --check\n",
+		},
+	}
+
+	for _, tCase := range tMatrix {
+		t.Run(tCase.Name, func(t *testing.T) {
+			require := require.New(t)
+
+			logfile := filepath.Join(t.TempDir(), "args.log")
+			t.Setenv("RPM_OSTREE_LOG_FILE", logfile)
+
+			if tCase.CheckExitCode != "" {
+				t.Setenv("RPM_OSTREE_CHECK_EXIT_CODE", tCase.CheckExitCode)
+			}
+			if tCase.UpgradeExitCode != "" {
+				t.Setenv("RPM_OSTREE_UPGRADE_EXIT_CODE", tCase.UpgradeExitCode)
+			}
+
+			rpmOstreeCMD, err := rpmostree.New("testdata/rpm-ostree.sh")
+			require.NoError(err, "Failed to create rpm-ostree command")
+
+			client, srv := NewFakeFleetlockServer(t, http.StatusOK)
+			t.Cleanup(srv.Close)
+
+			ctx, cancel := context.WithCancel(context.Background())
+			d := &daemon{
+				rpmostree:     rpmOstreeCMD,
+				checkInterval: 300 * time.Millisecond,
+				retryInterval: 100 * time.Millisecond,
+				ctx:           ctx,
+				cancel:        cancel,
+				fleetlock:     client,
+			}
+
+			done := make(chan struct{})
+			go func() {
+				d.watchForUpgrade()
+				close(done)
+			}()
+
+			// Wait for test to run
+			time.Sleep(150 * time.Millisecond)
+
+			d.cancel()
+			select {
+			case <-done:
+			case <-time.After(2 * d.checkInterval):
+				t.Fatalf("Timed out waiting for watchForUpgrade to exit")
+			}
+
+			buf, err := os.ReadFile(logfile)
+			require.NoError(err, "Failed to read logfile")
+
+			require.Equal(tCase.ExpectedCalls, string(buf), "Should have made the expected calls")
+		})
+	}
+}
 
 func TestDoUpgrade(t *testing.T) {
 	fakeDaemon := func(fleetlock *fleetlock.FleetlockClient, rpmostree *rpmostree.RPMOStreeCMD) *daemon {
